551f178d7e3e73474bd7ded9ab44ee7e3e964367
Dan Rabinowitz <djr@danielrabinowitz.com>
2012-03-26 20:47:46 -0600

    Use reject instead of delete_if in reporting

--- speedtest.rb
+++ speedtest.rb
@@ -6,39 +6,38 @@ require_relative 'config.rb'
 
 ###########
 # Set default configuration
-server_params = SpeedTestConfig.server_params || [
-  {:host => '192.168.1.1', :protocol => :afp},
-  {:host => '192.168.1.2', :protocol => :afp, :afp_volume => 'IT Files'}
-]
+server_params = SpeedTestConfig.server_params
 
 # 1 MB of random data
 transfer_file_params ||= {:bytes => 1*1000*1000, :type => :random}
 
 # Number of times to run the tests
-number_of_times_to_run_tests = 3
+number_of_times_to_run_tests = 1
 
 #########
-def shellescape(str)
-  # An empty argument will be skipped, so return empty quotes.
-  return "''" if str.empty?
-
-  str = str.dup
-
-  # Process as a single byte sequence because not all shell
-  # implementations are multibyte aware.
-  str.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1")
-
-  # A LF cannot be escaped with a backslash because a backslash + LF
-  # combo is regarded as line continuation and simply ignored.
-  str.gsub!(/\n/, "'\n'")
-
-  return str
-end
-
 def filesystem(path)
   return nil unless File::exists?(path)
   `df -m #{path} | tail --lines=+2 | cut -f1 -d' '`.strip
 end
+
+def umount(path)
+  filesystem = filesystem_initial = filesystem(path)
+  c = 0
+  begin
+    puts "umount: c=#{c}" if c > 0
+    break unless filesystem =~ /^afp_/
+
+    cmd = "umount #{path}"
+#    puts "cmd: #{cmd}"
+    system(cmd)
+    sleep 1
+    filesystem = filesystem(path)
+    break if filesystem != filesystem_initial
+    c+=1
+  end while c < 5
+  raise "Unable to unmount" unless c < 5
+end
+
 #########
 # Define class
 class Server
@@ -63,33 +62,42 @@ class Server
     puts "Connecting to #{@host} via #{@protocol}..."
     case @protocol
     when :afp
-      mount_point = "/Volumes/ServerSpeedTest_#{@host}"
+      mount_point = "/Volumes/#{@afp_volume}"
       filesystem_initial = filesystem(mount_point)
       if filesystem_initial
         if filesystem_initial =~ /^afp_/
           
-          abort "#{mount_point} is mounted already. In service to keeping this test reproducable, please unmount and then retry. Use:\numount #{mount_point}"
+          raise "#{mount_point} is mounted already. In service to keeping this test reproducable, please unmount and then retry. Use:\numount #{mount_point}"
         elsif filesystem_initial =~ /^\/dev\//
-          abort "#{mount_point} exists, but is not mounted. In service to keeping this test reproducable, please remove the directory and then retry. Use:\nrmdir #{mount_point}"
+          raise "#{mount_point} exists, but is not mounted. In service to keeping this test reproducable, please remove the directory and then retry. Use:\nrmdir #{mount_point}"
         else
           raise "Unhandled pattern for filesystem_initial: #{filesystem_initial}"
         end
       end
 
       # puts "Mounting volume at: #{mount_point}"
+      
+      cmd = "osascript -e 'tell application \"Finder\" to mount volume \"afp://#{@host}/#{@afp_volume}\"'"
+#      puts "cmd: #{cmd}"
+      `#{cmd}`
 
-      FileUtils.mkdir_p(mount_point)
-      cmd = "mount_afp -i \"afp://#{@username}\@#{@host}/#{@afp_volume}\" #{mount_point}"
-      puts "cmd: #{cmd}"
-      puts "Please enter the password for #{@username}"
-      system(cmd)
-
-      abort "Unable to mount #{mount_point}" if filesystem(mount_point) == filesystem_initial
+      raise "Unable to mount #{mount_point}" if filesystem(mount_point) == filesystem_initial
         
       @afp_destfile = "#{mount_point}/speedtest_temporary_destfile.#{$$}"
       @connected = true
     else
       raise "Attempt to connect with invalid protocol: #{@protocol}"
+    end
+  end
+
+  def disconnect
+    puts "Disconnecting from #{@host}..."
+    case @protocol
+    when :afp
+      mount_point = "/Volumes/#{@afp_volume}"
+      umount(mount_point)
+      # cmd = "sleep 5; osascript -e 'tell application \"Finder\" to eject \"#{@afp_volume}\"'; sleep 5"
+      # cmd = "sleep 5;umount -v #{mount_point}"
     end
   end
 
@@ -131,8 +139,10 @@ class Test
   
   def run
     @servers.each do |server|
+      server.connect
       @test_results << {:server => server, :upload_rate_bps => upload_rate_bps(server), :inplace_editing_ops_per_sec => inplace_editing_ops_per_sec(server), :download_rate_bps => download_rate_bps(server)}
       FileUtils.safe_unlink server.afp_destfile
+      server.disconnect
     end
     @test_results
   end
@@ -165,8 +175,8 @@ class Test
     ops = 100
     case server.protocol
     when :afp
-      time_start = Time.now
       file1 = File.new(server.afp_destfile, "r+")
+      time_start = Time.now
       ops.times do |n|
         file1.seek(Random.rand(@transfer_file.bytes - 1000))
         file1.getc
@@ -174,6 +184,7 @@ class Test
         file1.write(Array.new(1000) { rand(256) }.pack('c*'))    
       end
       time_finish = Time.now
+      file1.close
     end
     duration_seconds = (time_finish - time_start)
     ops / duration_seconds
@@ -210,12 +221,6 @@ end
 servers = Set.new
 server_params.each {|server_param| servers << Server.new(server_param)}
 
-begin
-  servers.each {|server| server.connect}
-rescue
-  puts "Failed to connect: #{$!}"
-end
-
 transfer_file = TestFile.new(transfer_file_params.merge({:basename => 'speedtest'}))
 
 test_results = []
@@ -224,13 +229,11 @@ number_of_times_to_run_tests.times do |n|
   Test.new(:servers => servers, :transfer_file => transfer_file).run.each {|r| test_results << r}
 end
 
-servers.each {|server| server.disconnect}
-
-#puts test_results.inspect
+puts test_results.inspect
 
 printf "%20s %15s %15s %30s\n", '', 'Upload (bps)', 'Download (bps)', 'Inplace Editing (ops / sec)'
 servers.each do |server|
-  results_for_server = test_results.delete_if {|r| r[:server] != server}
+  results_for_server = test_results.reject {|r| r[:server] != server}
   totals = results_for_server.inject {|sums, test_result| {:upload_rate_bps => sums[:upload_rate_bps] + test_result[:upload_rate_bps], :download_rate_bps => sums[:download_rate_bps] + test_result[:download_rate_bps], :inplace_editing_ops_per_sec => sums[:inplace_editing_ops_per_sec] + test_result[:inplace_editing_ops_per_sec]}}
   num = results_for_server.size
   printf "%20s %15.1f %15.1f %30.1f\n", server.host, totals[:upload_rate_bps] / num, totals[:download_rate_bps] / num, totals[:inplace_editing_ops_per_sec] / num
